bb3078d2b776b79f7e2585f923b37e8dc27d200a
miha0 <miha04udalov@gmail.com>
2025-10-15 23:32:10 +0300

    Объединение двух таблиц в одну merge и расчет коэффициентов для всего отдела

--- first.py
+++ first.py
@@ -55,3 +55,52 @@ def clean_financial_row(row):
 
 financial_cleaned = financial.apply(lambda x: clean_financial_row(x), axis=1)
 financial_cleaned = financial_cleaned.dropna(subset=['id'])
+
+
+# === 4. Объединение с таблицей пролонгаций ===
+merged = financial_cleaned.merge(prolong, on='id', how='left')
+
+
+
+# === 5. Расчёт коэффициентов пролонгации ===
+months = month_cols
+results = []
+
+
+for i in range(1, len(months)):
+    prev_m = months[i - 1]
+    curr_m = months[i]
+
+
+    # Первый месяц пролонгации
+    denom_df = merged[merged['month'] == prev_m] # проекты, завершившиеся в прошлом месяце
+    if len(denom_df) == 0:
+        continue
+
+
+    denom_sum = denom_df[prev_m].sum()
+    numer_sum = denom_df[curr_m].sum()
+    coef1 = numer_sum / denom_sum if denom_sum > 0 else 0
+
+
+    # Второй месяц пролонгации
+    prev_prev_m = months[i - 2] if i - 2 >= 0 else None
+    if prev_prev_m:
+        denom2_df = merged[(merged['month'] == prev_prev_m) & (merged[months[i - 1]] == 0)]
+        denom2_sum = denom2_df[prev_prev_m].sum()
+        numer2_sum = denom2_df[curr_m].sum()
+        coef2 = numer2_sum / denom2_sum if denom2_sum > 0 else 0
+    else:
+        coef2 = np.nan
+
+
+    results.append({
+        'month': curr_m,
+        'coef_first_month': round(coef1, 3),
+        'coef_second_month': round(coef2, 3)
+    })
+
+
+coef_df = pd.DataFrame(results)
+
+

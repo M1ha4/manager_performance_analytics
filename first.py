a6127483192ca041b8d54560fd73eac746bb9441
miha0 <miha04udalov@gmail.com>
2025-10-15 22:40:45 +0300

    Добавил в проект исходные csv файлы и загрузил их в pandas таблицы

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
-print('First file')
+import pandas as pd
+import numpy as np
+
+# === 1. Загрузка данных ===
+financial = pd.read_csv('data/financial_data.csv', sep=',')
+prolong = pd.read_csv('data/prolongations.csv', sep=',')
+
+# Приводим названия столбцов к нижнему регистру для удобства
+financial.columns = [col.strip().lower() for col in financial.columns]
+prolong.columns = [col.strip().lower() for col in prolong.columns]
+
+
